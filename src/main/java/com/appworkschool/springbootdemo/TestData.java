d2dba7df557ddf55f24fee5bc3fa26c09b40fa84
wei811006 <vloude0605@gmail.com>
2024-01-31 23:46:38 +0800

    remove redis

--- src/main/java/com/appworkschool/springbootdemo/TestData.java
+++ src/main/java/com/appworkschool/springbootdemo/TestData.java
@@ -5,11 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 

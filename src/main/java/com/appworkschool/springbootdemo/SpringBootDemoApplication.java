51e4414e17ec62b48435f011cd17a9fb1624597f
wei811006 <vloude0605@gmail.com>
2024-03-21 14:03:10 +0800

    Restart Test

--- src/main/java/com/appworkschool/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/appworkschool/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,14 @@ package com.appworkschool.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+		SpringApplication.exit(context);
 	}
 
 }
